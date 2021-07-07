508976c22633c6f416ce43767cb768db52d65cb5
Sadi_er59 <Sadi_er59@itstep.edu.az>
2021-07-06 22:37:37 -0700

    Add project files.

---
+++ notpad/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void toglebtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        public string flname { get; set; }
+        private void openbtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+            if (openFileDlg.ShowDialog() == true)
+            {
+                using (StringReader reader = new StringReader(openFileDlg.FileName))
+                {
+                    filetxtbox.Text = reader.ReadToEnd();
+                    flname = openFileDlg.FileName;
+                    NoteTxtBox.Text = File.ReadAllText(filetxtbox.Text);
+                }
+            }
+        }
+
+        private void Cutbtn_Click(object sender, RoutedEventArgs e)
+        {
+            NoteTxtBox.Cut();
+        }
+
+        private void Copybtn_Click(object sender, RoutedEventArgs e)
+        {
+            NoteTxtBox.Copy();
+        }
+
+        private void PasteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NoteTxtBox.Paste();
+        }
+
+        private void SelectAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NoteTxtBox.SelectAll();
+            NoteTxtBox.Focus();
+        }
+
+        private void XBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            using (StreamWriter streamWriter = new StreamWriter(flname))
+            {
+                streamWriter.Write(NoteTxtBox.Text);
+            }
+        }
+
+        private void NoteTxtBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (toglebtn.pressed == true)
+            {
+                SaveFileDialog save = new SaveFileDialog();
+
+                using (StreamWriter streamWriter = new StreamWriter(flname))
+                {
+                    streamWriter.Write(NoteTxtBox.Text);
+                }
+            }
+        }
+    }
+}
---
+++ notpad/UserControl1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        Thickness LeftSide = new Thickness(-39, 0, 0, 0);
+        Thickness RightSide = new Thickness(0, 0, -39, 0);
+        SolidColorBrush firstcolor = new SolidColorBrush(Color.FromRgb(250, 0, 0));
+        SolidColorBrush secondcolor = new SolidColorBrush(Color.FromRgb(130, 190, 125));
+        public bool pressed { get; set; } = false;
+        public UserControl1()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            pressed = false;
+            Dot.Margin = LeftSide;
+            Back.Fill = firstcolor;
+        }
+
+        private void Dot_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (pressed == false)
+            {
+                Dot.Margin = RightSide;
+                Back.Fill = secondcolor;
+                pressed = true;
+            }
+            else if (pressed)
+            {
+                Dot.Margin = LeftSide;
+                Back.Fill = firstcolor;
+                pressed = false;
+            }
+        }
+    }
+}
